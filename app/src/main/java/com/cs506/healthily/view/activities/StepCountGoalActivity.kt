1224060dcee2c57554ff245d97b8e560e07a5c1c
ekoranda1 <ekoranda1@gmail.com>
2021-11-17 23:18:51 -0600

    added import from google option in set step goal

--- app/src/main/java/com/cs506/healthily/view/activities/StepCountGoalActivity.kt
+++ app/src/main/java/com/cs506/healthily/view/activities/StepCountGoalActivity.kt
@@ -1,18 +1,30 @@
 package com.cs506.healthily.view.activities
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModelProviders
 import com.cs506.healthily.R
+import com.cs506.healthily.data.model.DaySteps
+import com.cs506.healthily.data.model.Goals
 import com.cs506.healthily.data.repository.GoalsRepository
+import com.cs506.healthily.viewModel.DayStepsViewModel
 import com.cs506.healthily.viewModel.goalViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.request.GoalsReadRequest
 import com.google.firebase.auth.FirebaseAuth
 
 class StepCountGoalActivity : AppCompatActivity() {
-
+    var TAG = "FIT"
     var age: String = ""
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step_count_goal)
@@ -30,8 +42,49 @@ class StepCountGoalActivity : AppCompatActivity() {
             finish()
         }
 
+        val importBtn : Button = findViewById(R.id.btn_import_from_google)
+        importBtn.setOnClickListener {
+            readStepGoal()
+        }
 
 
+
+
+    }
+
+    private val fitnessOptions: FitnessOptions by lazy {
+        FitnessOptions.builder()
+            .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+            .addDataType(DataType.TYPE_HEART_POINTS, FitnessOptions.ACCESS_READ)
+            .build()
+    }
+
+
+    private val stepReadRequest: GoalsReadRequest by lazy {
+        GoalsReadRequest.Builder()
+            .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
+            .build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun readStepGoal() {
+        Fitness.getGoalsClient(this, GoogleSignIn.getAccountForExtension(this, fitnessOptions))
+            .readCurrentGoals(stepReadRequest)
+            .addOnSuccessListener { goals ->
+                goals.firstOrNull()?.apply {
+                    val stepGoal = metricObjective.value
+
+                    Log.i(TAG, "Step Goal: $stepGoal")
+                    bindStepGoal(stepGoal.toInt().toString())
+                }
+            }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun bindStepGoal(goal: String) {
+        val viewModel: goalViewModel =
+            ViewModelProviders.of(this).get(goalViewModel::class.java)
+            viewModel.setStepGoal(goal)
 
     }
 
