20692d9d4a1fb0f72639f6a6a77729faa4da9637
armanramiz1 <aramiz@wisc.edu>
2021-11-18 04:12:51 -0600

    Small update to view, code refactoring

--- app/src/main/java/com/cs506/healthily/interfaces/SaveLocationInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.cs506.healthily.interfaces
-
-//import com.example.nearmekotlindemo.SavedPlaceModel
-
-interface SaveLocationInterface {
-
- //   fun onLocationClick(savedPlaceModel: SavedPlaceModel)
-}
