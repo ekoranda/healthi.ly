4fb64193b392496015858581869b87c844bcb211
unknown <jnpeplinski@wisc.edu>
2021-11-20 01:17:46 -0600

    Improved UI and added availability settings

--- app/src/main/java/com/cs506/healthily/data/model/UserSettings.kt
+++ app/src/main/java/com/cs506/healthily/data/model/UserSettings.kt
@@ -5,6 +5,8 @@ class UserSettings {
     var age: String? = null
     var weight: String? = null
     var height: String? = null
+    var availabilityStart: String? = null
+    var availabilityEnd: String? = null
 
     constructor(){}
 
@@ -12,11 +14,15 @@ class UserSettings {
         gender:String?,
         age: String?,
         weight: String?,
-        height: String?
+        height: String?,
+        availabilityStart: String?,
+        availabilityEnd: String?
     ){
         this.gender = gender
         this.age = age
         this.weight = weight
         this.height = height
+        this.availabilityStart = availabilityStart
+        this.availabilityEnd = availabilityEnd
     }
 }
--- app/src/main/java/com/cs506/healthily/view/activities/ProfileEditorActivity.kt
+++ app/src/main/java/com/cs506/healthily/view/activities/ProfileEditorActivity.kt
@@ -27,9 +27,14 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import android.widget.ArrayAdapter
+import com.cs506.healthily.view.fragments.mGoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
 
-
-
+lateinit var mGoogleSignInClient: GoogleSignInClient
 
 class ProfileEditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,9 +42,8 @@ class ProfileEditorActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile_editor)
         setUpSpinners()
 
-        val next : Button = findViewById<Button>(R.id.btn_next)
-
-        next.setOnClickListener {
+        val home : Button = findViewById<Button>(R.id.btn_home)
+        home.setOnClickListener {
             startActivity(
                 Intent(
                     this, MainActivity
@@ -48,6 +52,40 @@ class ProfileEditorActivity : AppCompatActivity() {
             )
             finish()
         }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        var currActivity: AppCompatActivity = this
+        mGoogleSignInClient = GoogleSignIn.getClient(currActivity, gso)
+        val logout: Button = findViewById<Button>(R.id.btn_log_out)
+        logout.setOnClickListener {
+            mGoogleSignInClient.signOut().addOnCompleteListener {
+                val intent = Intent(currActivity, SignInActivity::class.java)
+                Toast.makeText(currActivity, "Logging Out", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+
+            }
+        }
+        val fitnessOptions = FitnessOptions.builder()
+            .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+            .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+            .addDataType(DataType.TYPE_HEART_POINTS, FitnessOptions.ACCESS_READ)
+            .addDataType(DataType.AGGREGATE_HEART_POINTS, FitnessOptions.ACCESS_READ)
+            .build()
+        val disable: Button = findViewById<Button>(R.id.btn_disable_fit_permissions)
+        disable.setOnClickListener {
+            val fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.TYPE_HEART_POINTS, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_HEART_POINTS, FitnessOptions.ACCESS_READ)
+                .build()
+            val signInOptions = GoogleSignInOptions.Builder().addExtension(fitnessOptions).build()
+            val client = GoogleSignIn.getClient(currActivity, signInOptions)
+            client.revokeAccess()
+        }
+
     }
 
 
@@ -103,6 +141,17 @@ class ProfileEditorActivity : AppCompatActivity() {
 
         }
 
+
+
+
+
+
+
+
+
+
+
+
         val ages = resources.getStringArray(R.array.Ages)
         val ageSpinner = findViewById<Spinner>(R.id.sp_age)
         if (ageSpinner != null) {
@@ -111,18 +160,38 @@ class ProfileEditorActivity : AppCompatActivity() {
                 android.R.layout.simple_spinner_item, ages
             )
             ageSpinner.adapter = adapter
+
+            val goalViewModel: goalViewModel =
+                ViewModelProviders.of(this).get(goalViewModel::class.java)
+            goalViewModel.getUserSettings()?.observe(this) { goals ->
+                var currAge = goals.age.toString()
+//                Toast.makeText(this, gender, Toast.LENGTH_SHORT).show()
+                var ageIndex = 0
+                for (elem in ages.indices) {
+//                Toast.makeText(this, elem, Toast.LENGTH_SHORT).show()
+                    if (ages[elem].toString().contentEquals(currAge)) {
+                        ageIndex = elem
+                        ageSpinner.setSelection(ageIndex)
+//                    Toast.makeText(this, "Gender found!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
             ageSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                var selectionCount = 0
                 override fun onNothingSelected(parent: AdapterView<*>?) {
                     println("Nothing Selected")
                 }
 
                 override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
-                    val age = ages[position]
-                    val user = Firebase.auth.currentUser?.uid
-                    if (user != null) {
-                        bindAge(user, age)
+                    if (selectionCount > 0) {
+                        val age = ages[position]
+                        val user = Firebase.auth.currentUser?.uid
+                        if (user != null) {
+                            bindAge(user, age)
+                        }
                     }
+                    selectionCount++
                 }
 
             }
@@ -137,18 +206,38 @@ class ProfileEditorActivity : AppCompatActivity() {
                 android.R.layout.simple_spinner_item, weights
             )
             weightSpinner.adapter = adapter
+
+            val goalViewModel: goalViewModel =
+                ViewModelProviders.of(this).get(goalViewModel::class.java)
+            goalViewModel.getUserSettings()?.observe(this) { goals ->
+                var currWeight = goals.weight.toString()
+//                Toast.makeText(this, gender, Toast.LENGTH_SHORT).show()
+                var weightIndex = 0
+                for (elem in weights.indices) {
+//                Toast.makeText(this, elem, Toast.LENGTH_SHORT).show()
+                    if (weights[elem].toString().contentEquals(currWeight)) {
+                        weightIndex = elem
+                        weightSpinner.setSelection(weightIndex)
+//                    Toast.makeText(this, "Gender found!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
             weightSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                var selectionCount = 0
                 override fun onNothingSelected(parent: AdapterView<*>?) {
                     println("Nothing Selected")
                 }
 
                 override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
-                    val weight = weights[position]
-                    val user = Firebase.auth.currentUser?.uid
-                    if (user != null) {
-                        bindWeight(user, weight)
+                    if (selectionCount > 0) {
+                        val weight = weights[position]
+                        val user = Firebase.auth.currentUser?.uid
+                        if (user != null) {
+                            bindWeight(user, weight)
+                        }
                     }
+                    selectionCount++
                 }
 
             }
@@ -181,18 +270,151 @@ class ProfileEditorActivity : AppCompatActivity() {
                 android.R.layout.simple_spinner_item, heights
             )
             heightSpinner.adapter = adapter
+
+            val goalViewModel: goalViewModel =
+                ViewModelProviders.of(this).get(goalViewModel::class.java)
+            goalViewModel.getUserSettings()?.observe(this) { goals ->
+                var currHeight = goals.height.toString()
+//                Toast.makeText(this, gender, Toast.LENGTH_SHORT).show()
+                var heightIndex = 0
+                for (elem in heights.indices) {
+//                Toast.makeText(this, elem, Toast.LENGTH_SHORT).show()
+                    if (heights[elem].toString().contentEquals(currHeight)) {
+                        heightIndex = elem
+                        heightSpinner.setSelection(heightIndex)
+//                    Toast.makeText(this, "Gender found!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
             heightSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                var selectionCount = 0
                 override fun onNothingSelected(parent: AdapterView<*>?) {
                     println("Nothing Selected")
                 }
-
                 override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
-                    val height = heights[position]
-                    val user = Firebase.auth.currentUser?.uid
-                    if (user != null) {
-                        bindHeight(user, height)
+                    if (selectionCount > 0) {
+                        val height = heights[position]
+                        val user = Firebase.auth.currentUser?.uid
+                        if (user != null) {
+                            bindHeight(user, height)
+                        }
                     }
+                    selectionCount++
+                }
+
+            }
+        }
+
+        val times  = arrayOf("12am",
+            "1am",
+            "2am",
+            "3am",
+            "4am",
+            "5am",
+            "6am",
+            "7am",
+            "8am",
+            "9am",
+            "10am",
+            "11am",
+            "12pm",
+            "1pm",
+            "2pm",
+            "3pm",
+            "4pm",
+            "5pm",
+            "6pm",
+            "7pm",
+            "8pm",
+            "9pm",
+            "10pm",
+            "11pm"
+        )
+
+
+
+        val availabilityStartSpinner = findViewById<Spinner>(R.id.sp_availability_start)
+        if (availabilityStartSpinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, times
+            )
+            availabilityStartSpinner.adapter = adapter
+
+            val goalViewModel: goalViewModel =
+                ViewModelProviders.of(this).get(goalViewModel::class.java)
+            goalViewModel.getUserSettings()?.observe(this) { goals ->
+                var currStart = goals.availabilityStart.toString()
+//                Toast.makeText(this, gender, Toast.LENGTH_SHORT).show()
+                var startIndex = 0
+                for (elem in times.indices) {
+//                Toast.makeText(this, elem, Toast.LENGTH_SHORT).show()
+                    if (times[elem].toString().contentEquals(currStart)) {
+                        startIndex = elem
+                        availabilityStartSpinner.setSelection(startIndex)
+//                    Toast.makeText(this, "Gender found!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            availabilityStartSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                var selectionCount = 0
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    println("Nothing Selected")
+                }
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    if (selectionCount > 0) {
+                        val time = times[position]
+                        val user = Firebase.auth.currentUser?.uid
+                        if (user != null) {
+                            bindAvailabilityStart(user, time)
+                        }
+                    }
+                    selectionCount++
+                }
+
+            }
+        }
+
+        val availabilityEndSpinner = findViewById<Spinner>(R.id.sp_availability_end)
+        if (availabilityEndSpinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, times
+            )
+            availabilityEndSpinner.adapter = adapter
+
+            val goalViewModel: goalViewModel =
+                ViewModelProviders.of(this).get(goalViewModel::class.java)
+            goalViewModel.getUserSettings()?.observe(this) { goals ->
+                var currEnd = goals.availabilityEnd.toString()
+//                Toast.makeText(this, gender, Toast.LENGTH_SHORT).show()
+                var endIndex = 0
+                for (elem in times.indices) {
+//                Toast.makeText(this, elem, Toast.LENGTH_SHORT).show()
+                    if (times[elem].toString().contentEquals(currEnd)) {
+                        endIndex = elem
+                        availabilityEndSpinner.setSelection(endIndex)
+//                    Toast.makeText(this, "Gender found!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            availabilityEndSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                var selectionCount = 0
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    println("Nothing Selected")
+                }
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    if (selectionCount > 0) {
+                        val time = times[position]
+                        val user = Firebase.auth.currentUser?.uid
+                        if (user != null) {
+                            bindAvailabilityEnd(user, time)
+                        }
+                    }
+                    selectionCount++
                 }
 
             }
@@ -227,6 +449,20 @@ class ProfileEditorActivity : AppCompatActivity() {
 
         val aboutYouViewModel: AboutYou = ViewModelProviders.of(this).get(AboutYou::class.java)
         aboutYouViewModel.setWeightFromRepo(userId, weight)
+
+    }
+
+    private fun bindAvailabilityStart(userId: String, start: String) {
+
+        val aboutYouViewModel: AboutYou = ViewModelProviders.of(this).get(AboutYou::class.java)
+        aboutYouViewModel.setAvailabilityStartFromRepo(userId, start)
+
+    }
+
+    private fun bindAvailabilityEnd(userId: String, end: String) {
+
+        val aboutYouViewModel: AboutYou = ViewModelProviders.of(this).get(AboutYou::class.java)
+        aboutYouViewModel.setAvailabilityEndFromRepo(userId, end)
 
     }
 
