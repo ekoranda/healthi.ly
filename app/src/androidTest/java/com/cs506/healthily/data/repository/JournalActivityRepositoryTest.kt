2cf49125add6011f2653bad4e1efbc1c66ed96d1
ekoranda1 <ekoranda1@gmail.com>
2021-11-29 18:58:09 -0600

    Added tests to test new journal view model and journal data methods

--- app/src/androidTest/java/com/cs506/healthily/data/repository/JournalActivityRepositoryTest.kt
+++ app/src/androidTest/java/com/cs506/healthily/data/repository/JournalActivityRepositoryTest.kt
@@ -5,12 +5,14 @@ import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import com.cs506.healthily.data.model.JournalActivity
 import com.cs506.healthily.viewModel.AboutYou
 import com.cs506.healthily.viewModel.JournalViewModel
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Before
 
@@ -48,9 +50,43 @@ class JournalActivityRepositoryTest {
 
     }
 
+    @Test
+    fun deleteActivities(){
+        database.child("Users/$user/dailyActivity/fakeActivity/activity").setValue("fakeActivity")
+        repository.deleteJournal()
+        database.child("Users/$user").get().addOnSuccessListener{
+            Assert.assertFalse(it.child("dailyActivity").exists())
+
+        }.addOnFailureListener{
+
+        }
+
+    }
+
+    @Test
+    fun addActivity(){
+        val activity: JournalActivity = JournalActivity()
+        activity.stepCount = "3000"
+        activity.activity = "Test Activity"
+        activity.date = "Test Date"
+        activity.heartPoints = "40"
+
+        repository.addJournalActivity(activity)
+
+        database.child("Users/$user/dailyActivity").get().addOnSuccessListener{
+            Assert.assertTrue(it.child("Test Date").exists())
+            database.child("Users/$user/dailyActivity/Test Date").removeValue()
+
+
+
+        }.addOnFailureListener{
+
+        }
+    }
+    }
 
 
 
 
 
-}
+
